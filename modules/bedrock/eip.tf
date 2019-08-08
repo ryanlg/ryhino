87caa42258d44c1a3c693496fb005a381264a742
Ryan Liang <rl@ryanl.io>
2019-08-07 20:49:47 -0500

    Attach ngw to private subnets

--- modules/bedrock/eip.tf
+++ modules/bedrock/eip.tf
@@ -1,7 +1,15 @@
-resource "aws_eip" "ngw_bedrock" {
+resource "aws_eip" "ngw_bedrock_private_1a" {
   vpc = true
 
   tags = merge(var.global_tags, {
-    "Name" = "ryan-eip-ngw-bedrock"
+    "Name" = "ryan-eip-ngw-bedrock-private-1a"
+  })
+}
+
+resource "aws_eip" "ngw_bedrock_private_1b" {
+  vpc = true
+
+  tags = merge(var.global_tags, {
+    "Name" = "ryan-eip-ngw-bedrock-private-1b"
   })
 }
--- modules/bedrock/ngw.tf
+++ modules/bedrock/ngw.tf
@@ -1,6 +1,6 @@
-resource "aws_nat_gateway" "bedrock" {
+resource "aws_nat_gateway" "bedrock_private_1a" {
 
-  allocation_id = "${aws_eip.ngw_bedrock.id}"
+  allocation_id = "${aws_eip.ngw_bedrock_private_1a.id}"
   subnet_id     = "${aws_subnet.bedrock_private_1a.id}"
 
   depends_on = [
@@ -8,6 +8,20 @@ resource "aws_nat_gateway" "bedrock" {
   ]
 
   tags = merge(var.global_tags, {
-    "Name" = "ryan-ngw-bedrock"
+    "Name" = "ryan-ngw-bedrock-private-1a"
+  })
+}
+
+resource "aws_nat_gateway" "bedrock_private_1b" {
+
+  allocation_id = "${aws_eip.ngw_bedrock_private_1b.id}"
+  subnet_id     = "${aws_subnet.bedrock_private_1b.id}"
+
+  depends_on = [
+    "aws_internet_gateway.bedrock"
+  ]
+
+  tags = merge(var.global_tags, {
+    "Name" = "ryan-ngw-bedrock-private-1b"
   })
 }
